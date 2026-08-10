99c932505c09b3b5a2530a083bd40b3d47a78b7c
Rizki H.I <ibrahimriski211@gmail.com>
2026-08-10 21:00:30 +0900

    .

--- config/filesystems.php
+++ config/filesystems.php
@@ -38,14 +38,14 @@ return [
             'report' => false,
         ],
 
-        'public' => [
-            'driver' => 'local',
-            'root' => env('FILESYSTEM_PUBLIC_ROOT') ?: public_path('storage'),
-            'url' => rtrim(env('APP_URL', 'http://localhost'), '/') . '/storage',
-            'visibility' => 'public',
-            'throw' => false,
-            'report' => false,
-        ],
+      'public' => [
+    'driver' => 'local',
+    'root' => storage_path('app/public'),
+    'url' => rtrim(env('APP_URL', 'http://localhost'), '/') . '/storage',
+    'visibility' => 'public',
+    'throw' => false,
+    'report' => false,
+],
 
         's3' => [
             'driver' => 's3',
